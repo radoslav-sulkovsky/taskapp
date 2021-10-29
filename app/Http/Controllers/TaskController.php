8b9b305bac1f2096839eb8b341a969eaf5749a74
Radosław Sułkowski <radoslav.sulkovsky@gmail.com>
2021-10-29 19:07:35 +0200

    feat: create task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index() {
+        $result = Task::where('user_id', auth()->user()->id)
+            ->get();
+
+        return response()->json($result, 200);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:65535',
+        ]);
+
+        $result = Task::create([
+            'user_id' => auth()->user()->id,
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        return response()->json($result, 201);
+    }
+
+    public function show($id) {
+        $result = Task::where('id', $id)
+            ->where('user_id', auth()->user()->id)
+            ->first();
+
+        if(!$result) {
+            return response()->json(null, 404);
+        }
+
+        return response()->json($result, 200);
+    }
+
+    public function destroy($id) {
+        $result = Task::where('id', $id)
+            ->where('user_id', auth()->user()->id)
+            ->first();
+
+        if(!$result) {
+            return response()->json(null, 404);
+        }
+
+        $result->delete();
+
+        return response()->json(null, 204);
+    }
+}
