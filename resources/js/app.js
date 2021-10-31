4b816bdd0ac7200fc142c144e7aae53d5d37fe89
Radosław Sułkowski <radoslav.sulkovsky@gmail.com>
2021-10-31 11:45:54 +0100

    feat: create vue app main files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-require('./bootstrap');
+import Vue from "vue";
+import store from "./store";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+
+import App from "./components/App.vue"
+
+axios.get('/csrf-cookie').then(response => {
+    console.log('csrf cookie set');
+});
+
+const app = new Vue({
+    components: {
+        App
+    },
+    vuetify,
+    store,
+    router,
+    el: '#app'
+});
