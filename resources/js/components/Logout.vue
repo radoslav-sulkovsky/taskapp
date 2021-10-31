035bf696bd28e6b0b5952ecf5e2fe2fddb1d02c3
Radosław Sułkowski <radoslav.sulkovsky@gmail.com>
2021-10-31 11:46:27 +0100

    feat: create logout component

---
+++ resources/js/components/Logout.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('mounted logout');
+
+        this.$store.dispatch('user/logout').then(() => {
+            this.$router.push('/login');
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
