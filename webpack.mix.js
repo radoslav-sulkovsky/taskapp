2afa88ae04642a614e5e74fb2145de9345fd299f
Radosław Sułkowski <radoslav.sulkovsky@gmail.com>
2021-10-31 11:39:44 +0100

    feat: add vuetify support

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,47 @@
 const mix = require('laravel-mix');
 
+// webpack.config.js
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.s(c|a)ss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              // Requires sass-loader@^7.0.0
+              options: {
+                implementation: require('sass'),
+                indentedSyntax: true // optional
+              },
+              // Requires >= sass-loader@^8.0.0
+              options: {
+                implementation: require('sass'),
+                sassOptions: {
+                  indentedSyntax: true // optional
+                },
+              },
+            },
+          ],
+        },
+      ],
+    }
+  }
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
