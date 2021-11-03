6ffa7d08a6c5278f960d5ec9f238f27380306c32
Radosław Sułkowski <radoslav.sulkovsky@gmail.com>
2021-11-03 01:21:25 +0100

    feat: new template for login component

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,29 +1,13 @@
 <template>
-    <v-app>
-        <v-main>
-            <v-container>
-                <v-form ref="form" class="mx-4" lazy-validation>
-                    <v-text-field
-                        v-model="email"
-                        label="Username (e-mail)"
-                        placeholder="user@example.com"
-                        required
-                        :rules="validation.email"
-                    >
-                    </v-text-field>
-                    <v-text-field
-                        v-model="password"
-                        label="Password"
-                        placeholder="password"
-                        :type="'password'"
-                        required
-                        :rules="validation.password"
-                    ></v-text-field>
-                    <v-btn class="mt-2" color="primary" @click="login">Sign in</v-btn>
-                </v-form>
-            </v-container>
-        </v-main>
-    </v-app>
+    <div class="container">
+        <form class="form">
+            <label for="email">E-mail</label>
+            <input type="text" name="email" v-model="email">
+            <label for="password">Password</label>
+            <input type="password" name="password" v-model="password">
+        </form>
+        <button class="button" @click="login">Sign in</button>
+    </div>
 </template>
 
 <script>
@@ -32,15 +16,6 @@ export default {
         return {
             email: '',
             password: '',
-            validation: {
-                email: [
-                    v => !!v || 'E-mail is required',
-                    v => /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail must be valid',
-                ],
-                password: [
-                    v => !!v || 'Password is required',
-                ],
-            },
         }
     },
     computed: {
@@ -57,12 +32,6 @@ export default {
     },
     methods: {
         async login() {
-            let validate = this.$refs.form.validate();
-
-            if(!validate) {
-                return;
-            }
-
             const credentials = {
                 email: this.email,
                 password: this.password,
